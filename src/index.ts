7b079191b747e4264ed776da2328137b4cb39ac7
RumualdoPablo <pablorumualdo2305@gmail.com>
2023-09-11 18:09:13 -0300

    index.ts

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import * as dotenv from "dotenv";
+dotenv.config({ path: __dirname + "./../.env" });
+
+const app = express();
+const PORT = process.env.PORT ?? 4000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("¡Hola, mundo!");
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor running. PORT: ${PORT}`);
+});
+
